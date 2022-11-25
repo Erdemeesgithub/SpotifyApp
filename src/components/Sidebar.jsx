2a6d41bb99855623cde797a444b2eb436f544089
Erdem <110883062+UrCheryl@users.noreply.github.com>
2022-11-25 22:19:36 +0800

    done with the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,8 @@ import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { AiFillHome } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
 import { BiLibrary } from "react-icons/bi";
+import { AiFillPlusSquare } from "react-icons/ai";
+import { BiHeartSquare } from "react-icons/bi";
 
 export const Sidebar = () => {
   return (
@@ -10,26 +12,53 @@ export const Sidebar = () => {
       <div className={styles.top}>
         <div className={styles.buttons}>
           <BiDotsHorizontalRounded
-            style={{ color: "pink" }}
+            style={{ color: "#eee", width: 30, height: 30 }}
           ></BiDotsHorizontalRounded>
           <div className={styles.icon}>
-            <AiFillHome style={{ color: "white" }}></AiFillHome>
+            <AiFillHome
+              style={{ color: "white", width: 30, height: 30 }}
+            ></AiFillHome>
             <p>Home</p>
           </div>
           <div className={styles.icon}>
-            <FiSearch style={{ color: "white" }}></FiSearch>
+            <FiSearch
+              style={{ color: "white", width: 30, height: 30 }}
+            ></FiSearch>
             <p>Search</p>
           </div>
           <div className={styles.icon}>
-            <BiLibrary style={{ color: "white" }}></BiLibrary>
+            <BiLibrary
+              style={{ color: "white", width: 30, height: 30 }}
+            ></BiLibrary>
             <p>Your library</p>
           </div>
         </div>
         <div className={styles.create}>
-          <div className={styles.songs}></div>
+          <div className={styles.icon}>
+            <AiFillPlusSquare
+              style={{ color: "white", width: 30, height: 30 }}
+              className={styles.icon1}
+            ></AiFillPlusSquare>
+            <p>Create a playlist</p>
+          </div>
+          <div className={styles.icon}>
+            <BiHeartSquare
+              style={{ color: "white", width: 30, height: 30 }}
+            ></BiHeartSquare>
+            <p>Liked songs</p>
+          </div>
         </div>
       </div>
-      <div className={styles.bottom}></div>
+      <hr />
+      <div className={styles.bottom}>
+        <p>My Playlist #1</p>
+        <p>My Playlist #2</p>
+        <p>My Playlist #3</p>
+        <p>My Playlist #4</p>
+        <p>My Playlist #5</p>
+        <p>My Playlist #6</p>
+        <p>My Playlist #7</p>
+      </div>
     </div>
   );
 };
