ec7237c40a026a3dcd0131a8d89dcf1d1a8539e7
22PHOP2D-Erdemee <erdemee@LEAP-04s-iMac.local>
2022-11-29 19:32:02 +0800

    11.29

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
 import styles from "./../styles/Home.module.css";
+import { AiFillLeftCircle } from "react-icons/ai";
+import { AiFillRightCircle} from "react-icons/ai";
 
 export const Home = () => {
     return (
        <>
-       <div></div>
+       <div className={styles.container}>
+        <div className={styles.top}>
+            <div>
+                <AiFillLeftCircle></AiFillLeftCircle>
+                <AiFillRightCircle></AiFillRightCircle>
+            </div>
+            <button className={styles.upgrade}>Upgrade</button>
+        </div>
+        <div className={styles.picture}></div>
+        <div className={styles.play}></div>
+        <div className={styles.songs}></div>
+       </div>
        </>
     )
 }
