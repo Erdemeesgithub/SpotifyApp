c76b74905e72e5b64954f4a26bcbf94ebcc50c4f
22PHOP2D-Erdemee <erdemee@LEAP-04s-iMac.local>
2022-12-22 16:04:41 +0800

    messyhair

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="Container">
       <BrowserRouter>
-        <Sidebar />
+        <Sidebar></Sidebar>
         <Routes>
           <Route index element={<Home/>}/>
           <Route path="/Home" element={<Home/>}/>
